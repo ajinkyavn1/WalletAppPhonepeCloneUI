8a9e1773e020faca0e1b8615b2c5b5c46951b32e
Ajinkya Narkhede <ajinkyanarkhede7@gmail.com>
2021-03-11 00:00:31 +0530

    Payment Processing is Created

--- app/src/main/java/com/pict/walletappphonepeclone/Activityw/Add_Money.java
+++ app/src/main/java/com/pict/walletappphonepeclone/Activityw/Add_Money.java
@@ -2,15 +2,41 @@ package com.pict.walletappphonepeclone.Activityw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.pict.walletappphonepeclone.R;
 
 public class Add_Money extends AppCompatActivity {
-
+    Button ADDMONEY;
+    ProgressDialog pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__money);
+        pb=new ProgressDialog(this);
+        ADDMONEY=findViewById(R.id.Addmoney);
+        ADDMONEY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pb.setTitle("Payment Processing.....");
+                pb.show();
+                processPayment();
+            }
+        });
+    }
+
+    private void processPayment() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pb.cancel();
+            }
+        },5000);
+
     }
 }
