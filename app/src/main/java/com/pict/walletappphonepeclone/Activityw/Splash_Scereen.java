d87d12c22eecdd80ed4b81b293c9c871ec71d14c
Ajinkya Narkhede <ajinkyanarkhede7@gmail.com>
2021-03-10 12:58:59 +0530

    Splash Screen Created

---
+++ app/src/main/java/com/pict/walletappphonepeclone/Activityw/Splash_Scereen.java
@@ -0,0 +1,27 @@
+package com.pict.walletappphonepeclone.Activityw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.pict.walletappphonepeclone.MainActivity;
+import com.pict.walletappphonepeclone.R;
+
+public class Splash_Scereen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__scereen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        },5000);
+    }
+}
