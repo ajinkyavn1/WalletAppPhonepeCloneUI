11d72b4d7c94ee8318354214b1463fce041dedb8
Ajinkya Narkhede <ajinkyanarkhede7@gmail.com>
2021-03-13 01:56:47 +0530

    Profile UI Create

--- app/src/main/java/com/pict/walletappphonepeclone/Activityw/Register.java
+++ app/src/main/java/com/pict/walletappphonepeclone/Activityw/Register.java
@@ -3,16 +3,19 @@ package com.pict.walletappphonepeclone.Activityw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pict.walletappphonepeclone.R;
 
 public class Register extends AppCompatActivity {
-
+    EditText mobile,password,email,name;
+    Button Login,Submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+            mobile=findViewById(R.id.input_phone);
 
     }
 }
