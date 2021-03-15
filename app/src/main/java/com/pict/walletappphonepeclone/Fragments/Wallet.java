142ee26098f0c7c79f7dae7191a989b9d6156eb1
Ajinkya Narkhede <ajinkyanarkhede7@gmail.com>
2021-03-15 20:08:00 +0530

    QR code Scanner

--- app/src/main/java/com/pict/walletappphonepeclone/Fragments/Wallet.java
+++ app/src/main/java/com/pict/walletappphonepeclone/Fragments/Wallet.java
@@ -1,6 +1,7 @@
 package com.pict.walletappphonepeclone.Fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,9 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.MultiFormatUPCEANReader;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.pict.walletappphonepeclone.Activityw.Add_Money;
 import com.pict.walletappphonepeclone.R;
 
@@ -18,6 +28,7 @@ import com.pict.walletappphonepeclone.R;
 public class Wallet extends Fragment implements View.OnClickListener {
 
     View view;
+    ImageView Barcode;
     Button Addmoney,Transactions;
 
     public Wallet() {
@@ -31,10 +42,21 @@ public class Wallet extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_wallet, container, false);
         Addmoney=view.findViewById(R.id.Addmoney);
+        Barcode=view.findViewById(R.id.MYQR);
         Transactions=view.findViewById(R.id.Transactions);
         Addmoney.setOnClickListener(this);
         Transactions.setOnClickListener(this);
-    return view;
+        MultiFormatWriter wr=new MultiFormatWriter();
+        try {
+            BitMatrix bt=wr.encode("9881211594", BarcodeFormat.QR_CODE,350,350);
+            BarcodeEncoder barcodeEncoder=new BarcodeEncoder();
+            Bitmap bitmap=barcodeEncoder.createBitmap(bt);
+            Barcode.setImageBitmap(bitmap);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return view;
     }
 
     @Override
